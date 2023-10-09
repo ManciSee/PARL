eff16949e16b7771cd547d92f6537b16963bec79
Sergio Mancini <80248296+ManciSee@users.noreply.github.com>
2023-10-09 16:02:18 +0200

    Fix | Skip cookies

--- scrape/index.ts
+++ scrape/index.ts
@@ -1,17 +1,27 @@
 const puppeteer = require('puppeteer');
+
 const fs = require('fs');
 import { Browser } from 'puppeteer';
 
 const url = 'https://www.youtube.com/watch?v=OOtxXPaQvoM';
 
 const main = async () => {
-    const browser: Browser = await puppeteer.launch({ headless: false, executablePath: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser' });
+    const browser: Browser = await puppeteer.launch({ headless: false, timeout:0, executablePath: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser' });
     const page = await browser.newPage();
   await page.goto(url);
 
+  
+  // Cookie
+  //await page.click("#content > div.body.style-scope.ytd-consent-bump-v2-lightbox > div.eom-buttons.style-scope.ytd-consent-bump-v2-lightbox > div:nth-child(1) > ytd-button-renderer:nth-child(1) > yt-button-shape")
+  
+  // Ad
+   
   await page.waitForSelector('.ytp-caption-segment');
-
   const button = await page.evaluate('document.querySelector("button.ytp-subtitles-button").getAttribute("aria-pressed")');
+  
+  
+  
+
 
   if(button === 'false'){
     await page.click('.ytp-subtitles-button');
@@ -25,13 +35,12 @@ const main = async () => {
     );
     console.log(data);
 
-    
     fs.appendFile('transcript.json', JSON.stringify(data) + '\n', (err: any) => {
       if (err) throw err;
       console.log('ok');
     });
 
-    setTimeout(captureAndSaveWord, 3000);
+    setTimeout(captureAndSaveWord, 2000);
   };
 
   captureAndSaveWord();
