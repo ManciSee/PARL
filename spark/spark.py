31c70ff417d33a3ab96084c907357ae481ca684b
Sergio Mancini <80248296+ManciSee@users.noreply.github.com>
2023-11-02 12:01:31 +0100

    Add | Elasticsearch finally working!!!!!!!!!

--- spark/spark.py
+++ spark/spark.py
@@ -10,6 +10,7 @@ import csv
 import json
 from pyspark.sql.functions import from_json
 from pyspark.sql.types import StructType, StructField, StringType
+import pandas as pd
 
 def elaborate(batch_df: DataFrame, batch_id: int):
     batch_df.show(truncate=False)
@@ -47,16 +48,16 @@ es_mapping = {
 }
 
 
-es = Elasticsearch(elastic_host)
-response = es.indices.create(
-    index=elastic_index,
-    body=es_mapping,
-    ignore=400
-)
+#es = Elasticsearch(elastic_host)
+# response = es.indices.create(
+#     index=elastic_index,
+#     body=es_mapping,
+#     ignore=400
+# )
 
-if 'acknowledged' in response:
-    if response['acknowledged'] == True:
-        print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
+# if 'acknowledged' in response:
+#     if response['acknowledged'] == True:
+#         print ("INDEX MAPPING SUCCESS FOR INDEX:", response['index'])
 
 schema = StructType([
     StructField("id", StringType(), True),
@@ -78,6 +79,31 @@ def write_to_csv(record):
         writer = csv.writer(f)
         writer.writerow([record['id'], record['timestamp'], record['text'], record['duration']])
 
+def write_to_csv_and_send_to_es(record):
+    csv_file_path = "/app/transcription.csv"
+    column_names = ["id", "timestamp", "text", "duration"]
+
+    if not os.path.isfile(csv_file_path):
+        with open(csv_file_path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(column_names)
+    
+    with open(csv_file_path, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([record['id'], record['timestamp'], record['text'], record['duration']])
+    
+    # Create Elasticsearch client inside the function
+    es = Elasticsearch(elastic_host)
+    
+    # Send data to Elasticsearch
+    es_data = {
+        "id": record['id'],
+        "timestamp": record['timestamp'],
+        "text": record['text'],
+        "duration": record['duration']
+    }
+    es.index(index=elastic_index, body=es_data, ignore=400)
+
 print("Reading from Kafka...")
 df = spark \
     .readStream \
@@ -90,16 +116,10 @@ df = df.selectExpr("CAST(value AS STRING)")
 df = df.select(from_json("value", schema).alias("data"))
 df = df.select("data.id", "data.timestamp", "data.text", "data.duration")
 
-print("Save to CSV...")
+print("Save to CSV and send to Elasticsearch...")
 df.writeStream \
-    .foreach(write_to_csv) \
+    .foreach(write_to_csv_and_send_to_es) \
     .start() \
     .awaitTermination()
 print("Done!")
 
-print("Send to elasticsearch...")
-df.writeStream \
-    .option("checkpointLocation", "/save/location") \
-    .format("es") \
-    .start(elastic_index) \
-    .awaitTermination()
