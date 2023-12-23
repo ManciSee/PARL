d4a77682b7d40440ecd818783ccb06272dd51934
root <sergio201m@libero.it>
2023-12-23 18:28:16 +0100

    Fix sentiment analysis for all language

--- spark/spark.py
+++ spark/spark.py
@@ -1,120 +1,4 @@
-# from __future__ import print_function
-# import sys
-# import os
-# from pyspark import SparkContext, SparkConf
-# from pyspark.sql import SparkSession
-# from pyspark.streaming import StreamingContext
-# from pyspark.sql.dataframe import DataFrame
-# from elasticsearch import Elasticsearch
-# import csv
-# import json
-# from pyspark.sql.functions import from_json
-# from pyspark.sql.types import StructType, StructField, StringType
-# import pandas as pd
 
-# def elaborate(batch_df: DataFrame, batch_id: int):
-#     batch_df.show(truncate=False)
-
-# # Create a Spark context and session
-# SparkConf = SparkConf().set("es.nodes", "elasticsearch").set("es.port", "9200")
-# sc = SparkContext(conf=SparkConf, appName="StreamingKafka")
-# spark = SparkSession(sc)
-# sc.setLogLevel("ERROR")
-
-# # Kafka
-# kafkaServer = "kafka:39092"
-# topic = "speech"
-
-# # Elasticsearch
-# elastic_host = "http://elasticsearch:9200"
-# elastic_index = "speech"
-
-# es_mapping = {
-#     "mappings": {
-#         "properties": {
-#             "timestamp": {
-#                 "type": "date",
-#                 "format": "yyyy-MM-dd'T'HH:mm:ss"
-#             },
-#             "text": {
-#                 "type": "text",
-#                 "fielddata": True
-#             },
-#             "duration": {
-#                 "type": "float"
-#             }
-#         }
-#     }
-# }
-
-
-
-# schema = StructType([
-#     StructField("id", StringType(), True),
-#     StructField("timestamp", StringType(), True),
-#     StructField("text", StringType(), True),
-#     StructField("duration", StringType(), True)
-# ])
-
-# # def write_to_csv(record):
-# #     csv_file_path = "/app/transcription.csv"
-# #     column_names = ["id", "timestamp", "text", "duration"]
-
-# #     if not os.path.isfile(csv_file_path):
-# #         with open(csv_file_path, 'w', newline='') as f:
-# #             writer = csv.writer(f)
-# #             writer.writerow(column_names)
-    
-# #     with open(csv_file_path, 'a', newline='') as f:
-# #         writer = csv.writer(f)
-# #         writer.writerow([record['id'], record['timestamp'], record['text'], record['duration']])
-
-# def write_to_csv_and_send_to_es(record):
-#     csv_file_path = "/app/transcription.csv"
-#     column_names = ["id", "timestamp", "text", "duration"]
-
-#     if not os.path.isfile(csv_file_path):
-#         with open(csv_file_path, 'w', newline='') as f:
-#             writer = csv.writer(f)
-#             writer.writerow(column_names)
-    
-#     with open(csv_file_path, 'a', newline='') as f:
-#         writer = csv.writer(f)
-#         writer.writerow([record['id'], record['timestamp'], record['text'], record['duration']])
-    
-#     # Create Elasticsearch client inside the function
-#     es = Elasticsearch(elastic_host)
-    
-#     # Send data to Elasticsearch
-#     es_data = {
-#         "id": record['id'],
-#         "timestamp": record['timestamp'],
-#         "text": record['text'],
-#         "duration": record['duration']
-#     }
-#     es.index(index=elastic_index, body=es_data, ignore=400)
-
-# print("Reading from Kafka...")
-# df = spark \
-#     .readStream \
-#     .format("kafka") \
-#     .option("kafka.bootstrap.servers", kafkaServer) \
-#     .option("subscribe", topic) \
-#     .load()
-
-# df = df.selectExpr("CAST(value AS STRING)")
-# df = df.select(from_json("value", schema).alias("data"))
-# df = df.select("data.id", "data.timestamp", "data.text", "data.duration")
-
-# print("Save to CSV and send to Elasticsearch...")
-# df.writeStream \
-#     .foreach(write_to_csv_and_send_to_es) \
-#     .start() \
-#     .awaitTermination()
-# print("Done!")
-
-
-#--------------------------------------------------------------------------------------------------------------------
 from __future__ import print_function
 import sys
 import os
@@ -135,6 +19,8 @@ import re
 import numpy as np
 from typing import Set
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from googletrans import Translator
+from langdetect import detect
 
 
 def elaborate(batch_df: DataFrame, batch_id: int):
@@ -217,6 +103,9 @@ def write_to_csv_and_send_to_es(record):
     #Create an instance of the VADER sentiment analyzer
     analyzer = SentimentIntensityAnalyzer()
 
+    # Translate to en
+    translator = Translator()
+
     # topic modelling
     dataset = pd.read_csv(csv_file_path)
 
@@ -242,23 +131,28 @@ def write_to_csv_and_send_to_es(record):
         # 6 Topic
         top_terms = [term for term, _ in lda_model.show_topic(topic_index, topn=6) if not (term.isdigit() or '*' in term)]
 
-        # 1 Topic
-        unique_topic = top_terms[0] if top_terms else None
+        # Detect language for VADER, because it works better with en
+        detected_language = detect(record['text'])
+        if detected_language != 'en':
+            # If the detected language is not English, translate the text
+            translation = translator.translate(record['text'], dest='en').text
+        else:
+            # If the detected language is already English, use the original text
+            translation = record['text']
 
         # Sentiment
-        sentiment_score = analyzer.polarity_scores(record['text'])
+        sentiment_score = analyzer.polarity_scores(translation)
+
         # Check sentiment score range
         sentiment_text = get_sentiment_text(sentiment_score['compound'])
-
 
         results.append({
             'ID': row['id'],
             'Topic': topic_index,
             'Topic Score': topic_score,
             'Top Terms': ' , '.join(top_terms),
-            'Unique Topic': unique_topic,
             "Sentiment Score" : sentiment_score,
-            "Sentiment Text": sentiment_text
+            "Sentiment Text": sentiment_text,
         })
     print("Done!")
 
@@ -271,9 +165,8 @@ def write_to_csv_and_send_to_es(record):
         "topic": topic_index,
         "topic_score": topic_score,
         "top_terms": ', '.join(top_terms),
-        "unique_topic": unique_topic,
         "sentiment_score" : sentiment_score,
-        "sentiment_text": sentiment_text
+        "sentiment_text": sentiment_text,
     }
     es.index(index=elastic_index, body=es_data, ignore=400)
 
